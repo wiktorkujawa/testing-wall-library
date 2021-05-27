67e7d37127f7bdeee81081cacb963b539d40b0b4
wiktor <wiktorkujawa1993@gmail.com>
2021-05-27 20:54:20 +0200

    Added readme.md

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createMemoryHistory } from 'history'
+import { Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders Basic react(styled) link', () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  )
+  const linkElement = screen.getByText(/Basic/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('landing on a bad page', () => {
+  const history = createMemoryHistory()
+  history.push('/some/bad/route')
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  )
+
+  expect(screen.getByText(/Page not found./i)).toBeInTheDocument()
+})
